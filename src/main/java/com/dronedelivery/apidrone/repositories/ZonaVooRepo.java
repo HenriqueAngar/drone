64ad2a7cdb270696804b39b3371616e1b4958b23
lucasborba111 <lucasborgesborba@gmail.com>
2023-11-26 12:16:31 -0300

    Adicionando interface ZonaVoo

---
+++ src/main/java/com/dronedelivery/apidrone/repositories/ZonaVooRepo.java
@@ -0,0 +1,10 @@
+package com.dronedelivery.apidrone.repositories;
+
+import com.dronedelivery.apidrone.model.ZonaVoo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZonaVooRepo extends JpaRepository<ZonaVoo, Long>, CustomQuerydslPredicateExecutor<ZonaVoo>{
+
+}
