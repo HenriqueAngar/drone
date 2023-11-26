9944adc300c8efc792d39f1a1ae99074932134ad
lucasborba111 <lucasborgesborba@gmail.com>
2023-11-26 12:49:16 -0300

    adicionando status do drone

---
+++ src/main/java/com/dronedelivery/apidrone/model/StatusDrone.java
@@ -0,0 +1,12 @@
+package com.dronedelivery.apidrone.model;
+
+public enum StatusDrone {
+
+    CAIDO,
+    INOPERANTE,
+    FORA_DE_ALCANCE,
+    MANUTENCAO,
+    RUMO_BASE,
+    RUMO_COLETA,
+    RUMO_DESTINO
+}
